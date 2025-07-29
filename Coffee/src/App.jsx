d691981ffbe08116f3042e0005c744d9e49d181d
Avadh Golakiya <avadhgolakiya88@gmail.com>
2025-07-29 11:38:11 +0530

    again

--- Coffee/src/App.jsx
+++ Coffee/src/App.jsx
@@ -5,6 +5,8 @@ import LocationPage from './HomePage/LocationPage';
 import AboutPage from './HomePage/Aboutpage';
 import NewsPage from './HomePage/NewsPage';
 import './App.css';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 function App() {
   return (
